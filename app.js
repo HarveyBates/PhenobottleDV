644103982440f13d57a1d84eddcdcee1ce177d28
Harvey Bates <phenobottle@gmail.com>
2020-11-28 17:59:39 +1100

    work in progress

--- app.js
+++ app.js
@@ -1,46 +1,61 @@
-async function create_plot(name){
-    var trace1 = add_trace('Bottle 1', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+const {Client} = require('pg')
+const client = new Client({
+    user: "postgres",
+    password: "Surfingaus1",
+    host: "localhost",
+    port: 5432,
+    database: "test"
+})
 
-var data = [trace1];
+client.connect()
+.then(() => console.log("Connected"))
+.catch(e => console.log)
+.finally(() => client.end())
 
-var layout = { 
-    height: 300,
-    colorway : ['#f3cec9', '#cd7eaf', '#6f4d96', '#182844'],
-    title: 'Optical Density',
-    font: {size: 12, color: '#D2D1D1'},
-    paper_bgcolor: '#211F1F',
-    plot_bgcolor: '#2E2D2D',
-    showlegend: false, 
-    yaxis:{
-        title:{
-            text: 'Optical Density (OD)',
-        }
-    },
-    xaxis:{
-        title:{
-            text: 'Time (min)',
-        }
-    }
+async function create_plot(name, trace){
+    var data = [trace]
 
-};
+    var layout = { 
+        height: 300,
+        colorway : ['#f3cec9', '#cd7eaf', '#6f4d96', '#182844'],
+        title: 'Optical Density',
+        font: {size: 12, color: '#D2D1D1'},
+        paper_bgcolor: '#211F1F',
+        plot_bgcolor: '#2E2D2D',
+        showlegend: false, 
+        yaxis:{
+            title:{
+                text: 'Optical Density (OD)',
+            }
+        },
+        xaxis:{
+            title:{
+                text: 'Time (min)',
+            }
+        }, 
+        margin: {
+            l: 60,
+            r: 50,
+            b: 60,
+            t: 50,
+            pad: 2
+            },
+    };
 
+    var config = {responsive: true}
 
-var config = {responsive: true}
-
-Plotly.newPlot(name, data, layout, config );
-
+    Plotly.newPlot(name, data, layout, config );
 }
 
 function add_trace(name, xvalues, yvalues){
-    var trace1 = {
+    var trace = {
         fill: 'tozeroy',
         type: 'scatter',
         x: xvalues,
         y: yvalues,
         name: name
     };
-    return trace1;
+    return trace;
 }
 
 
