5a3586f55dfef3ce027c4af111739a20bf34bc6f
Harvey Bates <phenobottle@gmail.com>
2020-12-26 16:20:03 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const {Client} = require('pg')
 const client = new Client({
     user: "postgres",
-    password: "Surfingaus1",
+    password: "*********",
     host: "localhost",
     port: 5432,
     database: "test"
